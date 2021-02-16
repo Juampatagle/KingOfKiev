1c5e20257ab6748cf0791531bce8198009a92df2
Juampatagle <juampatagle@gmail.com>
2021-02-16 16:16:18 -0300

    agregue express

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-let fs = require('fs');
+//let http = require ('http');
 
-let moment = require('moment');
+//http.createServer(function(req, res) {
 
-let datos = fs.readFileSync(__dirname, '/prueba.txt', 'utf-8')
+//res.writeHead(200, {'Content-Type': 'text/plain'});
 
-console.log(datos);
+//switch(req.url){
+    //case '/':
+        //res.end("Bienvenido a KingOfKiev");
+        //break;
+   // case '/productos':
+        //res.end("Estos son nuestros productos");
+      //  break;
+   // default:
+      //  res.end("Error, intente de nuevo mas tarde");
+       // break;        
+//}
 
-console.log(moment().format('MMM Do YYYY'));
+//res.end('Terminado');
+
+//}).listen(4000, 'localhost');
+
+const express = require('express');
+const app = express();
+app.listen(3000,()=>{
+    console.log('Servidor corriendo');
+})
